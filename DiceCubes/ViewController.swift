437532cfcee32c5fd9c9a2d26e3a2c98f1bf5567
wwiku <viculova.anastasia@yandex.ru>
2020-12-10 00:36:59 +0300

    Hide label & add haptic

--- DiceCubes/ViewController.swift
+++ DiceCubes/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var ImageStackView: UIStackView!
     
@@ -21,52 +21,75 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
+    // Tapping on the screen
     
     override func touchesBegan (_ touches: Set<UITouch>, with event: UIEvent?) {
+        if #available(iOS 10.0, *) {
+            // Haptic feedback
+            let generator = UIImpactFeedbackGenerator(style: .light)
+            generator.prepare()
             self.diceRolling()
-    }
-    
-    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+            generator.impactOccurred()
+        } else {
+            // Fallback on earlier versions
             self.diceRolling()
         }
+    }
     
-    func diceRolling() {
-          
-        var milliseconds:Float = 8
-            
+    // Shake gesture
+    
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         if #available(iOS 10.0, *) {
-            Timer.scheduledTimer(withTimeInterval: 0.15, repeats: true) {
-                timer in
-                
-                let leftNumber = Int.random(in: 1...6)
-                let rightNumber = Int.random(in: 1...6)
-                                 
-                self.leftImage.image = UIImage(named: "Dice\(leftNumber)")
-                self.rightImage.image = UIImage(named: "Dice\(rightNumber)")
-                milliseconds = milliseconds - 1
-                    
-                if milliseconds <= 0 {
-                
-                    self.hintLabel.text = "\(leftNumber + rightNumber)"
-                    timer.invalidate()
-                    }
-                
-                }
-                // Fallback on earlier versions
-            
-            } else {
-                
-                let leftNumber = Int.random(in: 1...6)
-                let rightNumber = Int.random(in: 1...6)
-                              
-                self.leftImage.image = UIImage(named: "Dice\(leftNumber)")
-                self.rightImage.image = UIImage(named: "Dice\(rightNumber)")
+            //Haptic feedback
+            let generator = UIImpactFeedbackGenerator(style: .light)
+            generator.prepare()
+            self.diceRolling()
+            generator.impactOccurred()
+        } else {
+            // Fallback on earlier versions
+            self.diceRolling()
+        }
+    }
     
+    // Dice rolling function
+    
+func diceRolling() {
+    
+    var milliseconds:Float = 8
+    self.hintLabel.text = nil
+    
+    if #available(iOS 10.0, *) {
+        // Random dice animation
+        Timer.scheduledTimer(withTimeInterval: 0.15, repeats: true) {
+            timer in
+            
+            let leftNumber = Int.random(in: 1...6)
+            let rightNumber = Int.random(in: 1...6)
+            
+            self.leftImage.image = UIImage(named: "Dice\(leftNumber)")
+            self.rightImage.image = UIImage(named: "Dice\(rightNumber)")
+            milliseconds = milliseconds - 1
+            
+            if milliseconds <= 0 {
+                //
                 self.hintLabel.text = "\(leftNumber + rightNumber)"
-                    
-                }
-         }
+                timer.invalidate()
+            }
+        }
+        // Fallback on earlier versions
+        
+    } else {
+        
+        let leftNumber = Int.random(in: 1...6)
+        let rightNumber = Int.random(in: 1...6)
+        
+        self.leftImage.image = UIImage(named: "Dice\(leftNumber)")
+        self.rightImage.image = UIImage(named: "Dice\(rightNumber)")
+        
+        self.hintLabel.text = "\(leftNumber + rightNumber)"
+    }
 }
+}
+
